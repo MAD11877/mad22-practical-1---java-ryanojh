1fc44c2da5d8ecadeae9a0ce8c8815d5f15e171e
ryanojh <103872571+ryanojh@users.noreply.github.com>
2022-04-19 21:23:26 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -26,7 +26,29 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
+    List<Integer> nList = new ArrayList<Integer>();
+    int lowest = Integer.MAX_VALUE;
+    int ans = 0;
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter number: ");
+    int number = in.nextInt();
+    for (int i = number; i > 0; i--){
+      System.out.print("Enter number: ");
+      int x = in.nextInt();
+      nList.add(x);
+      }
+    for(int i : nList){
+      int count = 0;
+      for(int q : nList){
+        if (i != q){
+          count += 1;
+        }
+      }
+      if (count < lowest){
+        lowest = count;
+        ans = i;
+      }
+    }
+    System.out.println(ans);  
   }
 }
